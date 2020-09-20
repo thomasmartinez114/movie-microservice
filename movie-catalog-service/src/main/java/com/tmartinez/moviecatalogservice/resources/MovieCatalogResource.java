909a226efcf295104b0d95a6b187e9fe2721f42f
Thomas Martinez <tmartinez114@gmail.com>
2020-09-19 23:52:49 -0400

    Added dummy movie data

--- movie-catalog-service/src/main/java/com/tmartinez/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/tmartinez/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,10 +3,18 @@ package com.tmartinez.moviecatalogservice.resources;
 import com.tmartinez.moviecatalogservice.models.CatalogItem;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.catalog.Catalog;
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 public class MovieCatalogResource {
 
     public List<CatalogItem> getCatalog(String userId) {
+
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "Test", 4)
+        );
 
     }
 }
