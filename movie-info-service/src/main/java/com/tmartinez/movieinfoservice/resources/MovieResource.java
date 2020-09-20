17b37120ecb2bbdcfb3a697670d0a720cec2f250
Thomas Martinez <tmartinez114@gmail.com>
2020-09-20 00:04:35 -0400

    added RestController and Requestmapping to MovieResource

--- movie-info-service/src/main/java/com/tmartinez/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/tmartinez/movieinfoservice/resources/MovieResource.java
@@ -1,10 +1,16 @@
 package com.tmartinez.movieinfoservice.resources;
 
 import com.tmartinez.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/movies")
 public class MovieResource {
 
-    public Movie getmovieInfo() {
-
+    @RequestMapping("/{movieId}")
+    public Movie getmovieInfo(@PathVariable("movieId") String movieId) {
+        return new Movie(movieId, "Test name");
     }
 }
