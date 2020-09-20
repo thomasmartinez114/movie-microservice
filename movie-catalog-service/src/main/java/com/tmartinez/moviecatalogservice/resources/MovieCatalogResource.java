e891305b1aac525cac223f9eb486d785f9595b11
Thomas Martinez <tmartinez114@gmail.com>
2020-09-19 23:38:52 -0400

    CatalogItem class - with getter and settrs and constructor

--- movie-catalog-service/src/main/java/com/tmartinez/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/tmartinez/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,7 +1,12 @@
 package com.tmartinez.moviecatalogservice.resources;
 
+import com.tmartinez.moviecatalogservice.models.CatalogItem;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieCatalogResource {
+
+    public List<CatalogItem> getCatalog(String userId) {
+
+    }
 }
