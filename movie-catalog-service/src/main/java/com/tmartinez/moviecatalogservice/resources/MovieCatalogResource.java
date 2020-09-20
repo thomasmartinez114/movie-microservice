36177d8d8c906a44c9ca5b32fcda27427a9d2307
Thomas Martinez <tmartinez114@gmail.com>
2020-09-19 23:54:45 -0400

    added @RequestMapping aka routes for url path

--- movie-catalog-service/src/main/java/com/tmartinez/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/tmartinez/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,6 +1,8 @@
 package com.tmartinez.moviecatalogservice.resources;
 
 import com.tmartinez.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.catalog.Catalog;
@@ -8,9 +10,11 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource {
 
-    public List<CatalogItem> getCatalog(String userId) {
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         return Collections.singletonList(
                 new CatalogItem("Transformers", "Test", 4)
