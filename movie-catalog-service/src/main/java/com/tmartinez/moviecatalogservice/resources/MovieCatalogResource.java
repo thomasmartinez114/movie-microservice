53fd82c4bfc6987a158f5b9c20e8a52f346301a9
Thomas Martinez <tmartinez114@gmail.com>
2020-09-22 20:13:37 -0400

    hardcoded the movie rating

--- movie-catalog-service/src/main/java/com/tmartinez/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/tmartinez/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,11 +1,13 @@
 package com.tmartinez.moviecatalogservice.resources;
 
 import com.tmartinez.moviecatalogservice.models.CatalogItem;
+import com.tmartinez.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.catalog.Catalog;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,6 +19,10 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         // Get all rated movie IDs
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
 
         // For each movie ID, call movie info service and get details
 
